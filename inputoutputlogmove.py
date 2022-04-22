2d580a727cf96e7993a45041ca6dd6d5538e1f5c
salmazan97 <55813097+salmazan97@users.noreply.github.com>
2022-04-22 11:37:55 -0500

    Update inputoutputlogmove.py

--- inputoutputlogmove.py
+++ inputoutputlogmove.py
@@ -2,32 +2,32 @@ import os
 import shutil
 from datetime import date
 from pathlib import Path
-####inputtrace
+####
 def file_move_input():
     #move
-    srcf = '//gcait/c$/Program Files (x86)/NCR/BIS/Logs/InputTrace.log'
-    destf = 'S:\\ITMs\\BISTracelogs\\InputTraceLogsOLD\\'
+    srcf = 'filesource'
+    destf = 'filedestination'
     shutil.move(srcf, destf)
 
 def file_rename_input():
     grab_date = date.today()  # get date
     format_date = grab_date.strftime('%Y%m%d')  # format date into yyyymmdd
-    old = os.path.join('S:\\ITMs\\BISTracelogs\\InputTraceLogsOLD\\', 'InputTrace.log')
-    new = os.path.join('S:\\ITMs\\BISTracelogs\\InputTraceLogsOLD\\', 'InputTrace{date}.log'.format(date=format_date))
+    old = os.path.join('filedestination', 'filename')
+    new = os.path.join('filedestination', 'filename{date}'.format(date=format_date))
     os.rename(old,new)
 
-####outputtrace
+####
 def file_move_output():
     #move
-    srcf = '//gcait/c$/Program Files (x86)/NCR/BIS/Logs/OutputTrace.log'
-    destf = 'S:\\ITMs\\BISTracelogs\\'
+    srcf = 'filesource'
+    destf = 'filedestination'
     shutil.move(srcf, destf)
 
 def file_rename_output():
     grab_date = date.today()  # get date
     format_date = grab_date.strftime('%Y%m%d')  # format date into yyyymmdd
-    old = os.path.join('S:\\ITMs\\BISTracelogs\\OutputTraceLogsOLD\\', 'OutputTrace.log')
-    new = os.path.join('S:\\ITMs\\BISTracelogs\\OutputTraceLogsOLD\\', 'OutputTrace{date}.log'.format(date=format_date))
+    old = os.path.join('filedestination', 'filename')
+    new = os.path.join('filedestination', 'filename{date}'.format(date=format_date))
     os.rename(old,new)
 
 
