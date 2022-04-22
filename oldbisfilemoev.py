98233873fda672a6c0736c728a125381adcce896
salmazan97 <55813097+salmazan97@users.noreply.github.com>
2022-04-22 11:38:36 -0500

    Update oldbisfilemoev.py

--- oldbisfilemoev.py
+++ oldbisfilemoev.py
@@ -2,13 +2,13 @@ import os
 import shutil
 
 def file_move_bis():
-    dirpath = '//gcait/c$/Program Files (x86)/NCR/BIS/Logs/'
+    dirpath = 'directorypath'
     dirlist = os.listdir(dirpath)
 
     for filename in dirlist:
-        if 'BisTrace.txt2' in filename:
-            srcf = '//gcait/c$/Program Files (x86)/NCR/BIS/Logs/' + filename
-            destf = 'S:\\ITMs\\BISTracelogs\\BISTraceLogsOLD\\'
+        if 'string' in filename:
+            srcf = 'source' + filename
+            destf = 'destination'
             shutil.move(srcf, destf)
             print('move complete')
         else:
